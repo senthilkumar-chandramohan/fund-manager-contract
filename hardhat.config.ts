16b4b64f401d7627d6cb22c26d0ff2a22f0b82a3
Senthilkumar Chandramohan <senthilkumar.chandramohan@outlook.com>
2026-02-14 23:45:49 +0530

    Updated config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,10 @@
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const ETHERSCAN_API_KEY = "VVCPGMJQZ2IZ2FQUT7K79HS2K4SEA6AVB9";
-const INFURA_API_KEY = "d0c6d0a0d17d41aa9967ad0a0f438570";
-const SEPOLIA_WALLET_PRIVATE_KEY = "98aef9b63894fae74ac3818e3c987391e75114688644841e2c405fdc9d0ca2df";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY ?? "";
+const INFURA_API_KEY = process.env.INFURA_API_KEY ?? "";
+const SEPOLIA_WALLET_PRIVATE_KEY = process.env.SEPOLIA_WALLET_PRIVATE_KEY ?? "";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -22,7 +23,7 @@ const config: HardhatUserConfig = {
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_WALLET_PRIVATE_KEY]
+      accounts: SEPOLIA_WALLET_PRIVATE_KEY ? [SEPOLIA_WALLET_PRIVATE_KEY] : []
     }
   }
 };
